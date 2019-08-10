a180a1c6b90f0082257482a1bd864dc87bc38d7c
Jordan Hayes <jordanhayes@Jordans-MacBook-Pro.local>
2019-08-09 22:15:04 -0500

    Updates

---
+++ precache-manifest.75fdcb7bfdcf6d66aabb6b940ecb9eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769dbc247beac6b92c41b8b1e6837398",
+    "url": "/mern-react-app/index.html"
+  },
+  {
+    "revision": "27fbf3fb3f6f1e3b5464",
+    "url": "/mern-react-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b6c0476ce0d4f611a641",
+    "url": "/mern-react-app/static/css/main.5231334a.chunk.css"
+  },
+  {
+    "revision": "27fbf3fb3f6f1e3b5464",
+    "url": "/mern-react-app/static/js/2.4310f214.chunk.js"
+  },
+  {
+    "revision": "b6c0476ce0d4f611a641",
+    "url": "/mern-react-app/static/js/main.61044126.chunk.js"
+  },
+  {
+    "revision": "6fdcc1c5e8876d33e057",
+    "url": "/mern-react-app/static/js/runtime~main.52d22086.js"
+  }
+]);
